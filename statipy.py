8a8945908e4f50f3e8c2d13110f6a589a64dc2c4
mxayon <mxnkpl@gmail.com>
2019-02-17 23:44:42 -0800

    discogs class and auth steps

--- statipy.py
+++ statipy.py
@@ -3,3 +3,14 @@ import spotipy
 import json
 from json.decoder import JSONDecodeError
 import spotipy.util as util
+
+class Discogs:
+    def __init__(self, album, artist):
+        self.album = album
+        self.artist = artist
+
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        username = sys.argv[1]
+        scope = "user-read-private user-read-email user-top-read user-library-read"
