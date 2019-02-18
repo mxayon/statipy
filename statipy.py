a31c8725b655b3f637f08f218c7fe01969c1bfff
mxayon <mxnkpl@gmail.com>
2019-02-18 13:28:06 -0800

    EUREKA/! playlists shows tracks and tracks shows artists

--- statipy.py
+++ statipy.py
@@ -4,23 +4,30 @@ import json
 from json.decoder import JSONDecodeError
 import spotipy.util as util
 
-class Discogs:
-    def __init__(self, album, artist):
-        self.album = album
-        self.artist = artist
-
 def show(tracks):
     for i, item in enumerate(tracks['items']):
         track = item['track']
         track_key = track['id']
         track_results = stp.track(track_key)
+        artist_info = track_results['artists']
+        artist_key = artist_info[0]['id']
         track_ids.append(track_key)
-        # print( " {} {} {}".format(i, track['artists'][0]['name'], track['name']))
+        artist_ids.append(artist_key)
+        print( " {} {} {}".format(i, track['artists'][0]['name'], track['name']))
+        print()
 
 def show_track_artist():
-    print()
-def show_track_album():
-    print()
+    for item in range(len(track_ids)):
+        track_key = track_ids[item]
+        track_results = stp.track(track_key)
+        print("Song: {} | {}  |".format(track_results['name'], track_results['id']))
+        print("Song popularity: {} Contains explicit content? {}".format(track_results['popularity'], track_results['explicit']))
+        artist_info = track_results['artists']
+        artist_name = artist_info[0]['name']
+        artist_key = artist_info[0]['id']
+        print("Artist: {} | {}  |".format(artist_name, artist_key))
+        artist_results = stp.artist(artist_key)
+        print(json.dumps(artist_results, sort_keys=True, indent=4))
 
 
 
@@ -28,7 +35,7 @@ if __name__ == '__main__':
     if len(sys.argv) > 1:
         username = sys.argv[1]
         scope = "user-read-private user-read-email user-top-read user-library-read"
-        print("|| Hello," + username + " STATIPY world! || ")
+        print("|| Hello, -" + username + " - Welcome to STATIPY world! || ")
     else:
         print("ALERT - NOT AUTHORIZED")
         print("usage: python user_playlists.py [username]")
@@ -47,23 +54,26 @@ if __name__ == '__main__':
         playlists = stp.user_playlists(username)
         # Set.. Go!
         track_ids = []
-        artists_ids = []
+        artist_ids = []
         album_ids = []
         ready = 0
         # Loops through playlists
         for playlist in playlists['items']:
             if playlist['owner']['id'] == username:
-                print(playlist['name'])
-                print("   total tracks", playlist['tracks']['total'])
+                # print(playlist['name'])
+                # print("   total tracks", playlist['tracks']['total'])
                 results = stp.user_playlist(username, playlist['id'], fields="tracks,next")
                 tracks = results['tracks']
                 print()
                 print()
 
                 show(tracks)
-                print(len(track_ids))
 
             # while tracks >> next
+                while tracks['next']:
+                    tracks = stp.next(tracks)
+                    show(tracks)
+                    show_track_artist()
     else:
         print("Token not cool. Auth Required.")
 
